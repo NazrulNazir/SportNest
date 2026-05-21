217674357017b360837e3997e871f3efb8a01901
Nazrul Nazir <nazrul6290@gmail.com>
2026-05-21 12:25:11 +0600

    complete my boooking page style..

--- src/app/mybookings/page.jsx
+++ src/app/mybookings/page.jsx
@@ -2,10 +2,13 @@ import BookingEmpty from '@/components/BookingEmpty';
 import { auth } from '@/lib/auth';
 // import { useSession } from '@/lib/auth-client';
 import { getMybooking } from '@/lib/data';
+import { Button } from '@heroui/react';
 import { headers } from 'next/headers';
 import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
+import { FaRegClock } from 'react-icons/fa';
+import { MdDateRange } from 'react-icons/md';
 
 const MyBookingsPage = async () => {
 
@@ -23,12 +26,39 @@ const MyBookingsPage = async () => {
     return <BookingEmpty />
   }
   return (
-    <div className='max-w-7xl mx-auto px-5 sm:px-0 mt-10'>
+    <div className='mx-auto px-5 sm:px-0 mt-10 max-w-5xl'>
       <h1 className='text-2xl font-bold text-gray-800'>My Bookings</h1>
-      <div>
+      <div className='flex flex-col gap-4 mt-10'>
         {
           bookings.map(booking => (<div key={booking._id}>
-            <Image width={100} height={100} src={booking.imageUrl} alt='hello'></Image>
+            <div className='bg-gray-100 rounded-xl py-3 px-4 flex flex-col sm:flex-row justify-between sm:items-center gap-5'>
+
+              <div className='flex gap-5'>
+                <Image width={200} height={120} className='rounded-lg' src={booking.imageUrl} alt='hello'></Image>
+                <div>
+                  <div className='sm:flex items-center gap-5 mb-4'>
+                    <h2 className='text-lg font-bold text-gray-800 mb-5 sm:-0'>{booking.facilityName}</h2>
+                    <span className='bg-yellow-50 px-4 py-1.5 rounded-full font-semibold text-yellow-500'>pending</span>
+                  </div>
+                  <div className='flex flex-col sm:flex-row gap-2 sm:gap-4 sm:mt-3'>
+                    {/* booking data, timeslot, price */}
+                    <p className='text-gray-600 font-semibold flex gap-1 items-center'>
+                      <span className='text-green-500'><MdDateRange /></span>
+                      {booking.facilityDate}
+                    </p>
+                    <p className='text-gray-600 font-semibold flex gap-1 items-center'>
+                      <span className='text-green-500'><FaRegClock /></span>
+                      {booking.timeSlot}</p>
+                    <p className='font-semibold text-gray-600'>${booking.price}</p>
+
+                  </div>
+                </div>
+              </div>
+
+                <Button className={'text-lg font-semibold text-red-500 bg-gray-200 w-full sm:w-25'}>Cancel</Button>
+              
+            </div>
+
           </div>))
         }
       </div>
---
+++ src/components/CancelBokking.jsx
@@ -0,0 +1,39 @@
+"use client";
+import {AlertDialog, Button} from "@heroui/react";
+
+const CancelBokking = () => {
+    return (
+        <div>
+            <AlertDialog>
+                <Button variant="secondary" className={'text-red-500'}>Cancel Booking</Button>
+                <AlertDialog.Backdrop>
+                    <AlertDialog.Container>
+                        <AlertDialog.Dialog className="sm:max-w-100">
+                            <AlertDialog.CloseTrigger />
+                            <AlertDialog.Header>
+                                <AlertDialog.Icon status="danger" />
+                                <AlertDialog.Heading>Cancel Booking permanently?</AlertDialog.Heading>
+                            </AlertDialog.Header>
+                            <AlertDialog.Body>
+                                <p>
+                                    This will permanently delete <strong>My Awesome Project</strong> and all of its
+                                    data. This action cannot be undone.
+                                </p>
+                            </AlertDialog.Body>
+                            <AlertDialog.Footer>
+                                <Button slot="close" variant="tertiary">
+                                    Cancel
+                                </Button>
+                                <Button slot="close" variant="danger">
+                                    Cancel Booking
+                                </Button>
+                            </AlertDialog.Footer>
+                        </AlertDialog.Dialog>
+                    </AlertDialog.Container>
+                </AlertDialog.Backdrop>
+            </AlertDialog>
+        </div>
+    )
+}
+
+export default CancelBokking
